7b8129a6635afa98be1b2f27fa4160e1a5451b1d
CesarDanielOC <146128780+CesarDanielOC@users.noreply.github.com>
2024-09-06 09:42:59 -0600

    Add files via upload

---
+++ cadenas.py
@@ -0,0 +1,31 @@
+'''Olave Cruz 
+Trabajando con Cadenas.'''
+
+Nombre="Daniel"
+print (Nombre[0])
+print (Nombre)
+
+
+MisNovias=["Agripina","Anastacia","Maria"]
+for Novias in MisNovias:
+    print(Novias)
+
+
+b = "Daniel Olave"
+print ("Otro Ejemplo")
+print (b[2:5])
+print("a mayuskula")
+a =("PiLiN dE PeRrO")
+print(a.upper())
+print("concatenar")
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
+
+
+
+
+"a2_v_c_if_for_while_nava"  """p2_v_c_if_for_while"""
