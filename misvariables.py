ce81f0edff296c1d17bfb43c674206905e6c0e10
CesarDanielOC <146128780+CesarDanielOC@users.noreply.github.com>
2024-09-06 09:59:56 -0600

    Add files via upload

---
+++ misvariables.py
@@ -0,0 +1,16 @@
+import math
+edad=17
+nombre="Daniel"
+print("tu edad es", edad)
+print("tu nombre es", nombre)
+print("--- Tipo de Variables ---")
+x=str(3)
+y=int(3)
+z=float(3)
+
+print(x)
+print(y)
+print(z)
+print("---Funciones Matemáticas")
+x=math.sqrt(64)
+print(x)
